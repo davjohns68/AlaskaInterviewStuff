33267052679d596dbf2109c9657adfe7a2f8dfcd
David Johnson <davjohns@outlook.com>
2017-10-24 09:27:53 -0700

    Initial commit

---
+++ src/alaskainterviewstuff/AlaskaInterviewStuff.java
@@ -0,0 +1,21 @@
+/*
+ * <Enter Description>
+ * Created by David Johnson, <Date>
+ * for CS211 course, Bellevue College
+ */
+package alaskainterviewstuff;
+
+/**
+ *
+ * @author davjohns
+ */
+public class AlaskaInterviewStuff {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
